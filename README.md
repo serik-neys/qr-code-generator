9bf48189e2f068e5a36c5b83dffd966b6df698d4
Serik_neys <muratserikfree@gmail.com>
2022-10-11 13:48:39 +0300

    tets

--- README.md
+++ README.md
@@ -14,17 +14,3 @@ Instal All dependencies in this project
 
 Runs the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-### Link
-
-Reactjs: https://reactjs.org/docs/create-a-new-react-app.html
-QrCode Tiger: https://www.qrcode-tiger.com/
-QrCode Tiger Doc: https://www.qrcode-tiger.com/api-documentation
-QrCode Tiger Stoplight: https://qrtiger.stoplight.io/docs/qrtiger-api/b3A6NzgwMTkwNg-create-static-qr-code
-TailwindCSS: https://tailwindcss.com/
-React Color: https://casesandberg.github.io/react-color/#examples
-File Saver: https://www.npmjs.com/package/file-saver
-
-### Video Tutorial
-
-You can see my youtube video for this project in [here](https://youtu.be/z_CB9-2zzdY)
--- src/App.js
+++ src/App.js
@@ -1,65 +1,27 @@
-import axios from "axios";
-import { createContext, useState } from "react";
-import InputForm from "./components/InputForm";
-import QrCode from "./components/QrCode";
-
-// Create context
-export const InputContext = createContext();
+import React, { useState } from 'react'
+import QRCode from "react-qr-code"
+//https://github.com/TylerPottsDev/yt-react-qr-generator
 
 function App() {
-  const [inputValue, setInputValue] = useState({
-    url: '',
-    color: ''
-  });
-  const [response, setResponse] = useState('');
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const config = {
-    headers: { Authorization: 'Bearer e0e29170-9e80-11ec-8ef8-55375f08d076' }
-  }
-  const bodyParameters = {
-    "colorDark": inputValue.color,
-    "qrCategory": "url",
-    "text": inputValue.url
-  }
-  const getQrCode = async () => {
-    try {
-      setLoading(true);
-      const res = await axios.post(
-        'https://qrtiger.com/api/qr/static',
-        bodyParameters,
-        config
-      );
-      setResponse(res.data.url);
-    } catch(err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  const value = {
-    inputValue,
-    setInputValue,
-    getQrCode,
-    response,
-    loading,
-    error
-  }
-
+  const [value, setValue] = useState('')
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-24 md:pt-80 px-2">
-      <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
-        <div className="md:grid md:grid-cols-3">
-          <InputContext.Provider value={value}>
-            <InputForm />
-            <QrCode />
-          </InputContext.Provider>
-        </div>
-      </div>
+    <div className='container mx-auto w-2/4'>
+      <h1 className='py-4 font-bold text-center text-2xl'>Student Qt code</h1>
+      <p className='mb-4'>Инструкциясы: Вебсайттын ссылкасын қойыңыз</p>
+      <input
+      onChange={(e) => setValue(e.target.value) }
+      value={value}
+      type="text"
+      placeholder="Введите ссылку"
+      className="border border-gray-300 w-full h-8 p-4 font-light text-sm focus"
+      />
+      <QRCode 
+      className='mx-auto mt-20'
+      value={value}
+      />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
